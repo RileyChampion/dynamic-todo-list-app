ee6f4893bd8a622a891fbcbb22f8f7f8242c81c4
Riley Champion <rchamp2u@gmail.com>
2025-05-04 02:18:53 -0700

    Add CORS policies (allow all for now)

--- TodoApp.API/Program.cs
+++ TodoApp.API/Program.cs
@@ -5,10 +5,20 @@ using TodoApp.API.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Add Cors Policies
+builder.Services.AddCors(options => {
+   options.AddPolicy("AllowAll", policy => {
+    policy.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyHeader();
+   });
+});
+// Add Feature Factory to Scope
 builder.Services.AddScoped<TodoFeatureFactory>();
+// Add Controllers to services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<TodoAppDbContext>(options => 
@@ -24,6 +34,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 
 app.MapControllers();
 
